bb1e18d2f4ba1ce5b4a29b865896f5e9dc9fea05
masatoyasuda <55440519+masatoyasuda@users.noreply.github.com>
2024-03-19 14:26:45 +0900

    Create work-date-update.php

---
+++ server/work-date-update.php
@@ -0,0 +1,34 @@
+<?php
+    try{
+        require('./active-record.php');
+        require('./get-record.php');
+
+        $patch_data = json_decode(file_get_contents('php://input'), true);
+        $db = new GetRecord('./../db/db.sqlite3');
+        
+        $err_msg = $db->recordValidate('work', $patch_data);
+        $json = [];
+        if(is_null($err_msg)){
+            $now = date("Y-m-d H:i");
+            $stmt = $db->pdo->prepare('
+                UPDATE work
+                SET started_date = :started_date, completed_date = :completed_date, updated_at = :updated_at
+                WHERE id = :id
+            ');
+            $stmt->bindParam(':started_date', $patch_data['started_date'], PDO::PARAM_STR);
+            $stmt->bindParam(':completed_date', $patch_data['completed_date'], PDO::PARAM_STR);
+            $stmt->bindParam(':updated_at', $now, PDO::PARAM_STR);
+            $stmt->bindParam(':id', $patch_data['id'], PDO::PARAM_INT);
+            if($stmt->execute()){
+                $json['res'] = 'OK';
+            } else {
+                throw new PDOException('保存に失敗しました');
+            }
+        } else {
+            $json['err'] = $err_msg;
+        }
+        echo json_encode($json);
+    } catch(PDOException $e){
+        echo json_encode(['err' => $e->getMessage()]);
+    }
+?>
